0a1e1cd5250b3aac0298b0b20874e719a7245c12
Edgar <soloviev.ed16@gmail.com>
2025-05-05 14:35:23 +0300

    Add support for sorting user wallets by name
    Updated DTO and enums to include 'name' as a sort parameter. Enhanced the service to enable sorting by name and adjusted item ordering when this option is used.

--- src/user-wallet/user-wallet.service.ts
+++ src/user-wallet/user-wallet.service.ts
@@ -178,7 +178,7 @@ export class UserWalletService {
       userWallet['balance'] = wallet.balance;
       userWallet.creationDate = this.getWalletAge(wallet.creationDate);
       userWallet.pnl = {
-        percent: wallet.pnlPercentage,
+        percent: wallet.pnl ? wallet.pnlPercentage : 0,
         value: wallet.pnl,
       };
       userWallet.winrate = wallet.winrate;
